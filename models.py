bc01b2abc7561fc077f939d526a5d36e46ebf0d3
witmus <witold.muszynski123@wp.pl>
2025-05-20 07:31:35 +0200

    fix LSTM outputs

--- models.py
+++ models.py
@@ -15,12 +15,8 @@ class LSTM(nn.Module):
         self.out = nn.Linear(n_hidden,1)
 
     def forward(self, x, state=None):
-        r_out, (h_s, c_s) = self.lstm(x, state)
-        outs = []
-        for ts in range(r_out.size(1)):
-            outs.append(self.out(r_out[:,ts,:]))
-
-        return torch.stack(outs, dim=1), h_s, c_s
+        lstm_out, (h_s, c_s) = self.lstm(x, state)
+        return self.out(lstm_out), h_s, c_s
 
 class SingleConvLSTM(nn.Module):
     def __init__(self, n_conv_outs, s_kernel, n_stride, n_hidden, n_layers):
@@ -47,11 +43,7 @@ class SingleConvLSTM(nn.Module):
         x = self.conv1(x)
         x = x.permute(0, 2, 1)
         lstm_out, (h_s, c_s) = self.lstm(x, state)
-        outs = []
-        for ts in range(lstm_out.size(1)):
-            outs.append(self.out(lstm_out[:,ts,:]))
-
-        return torch.stack(outs, dim=1), h_s, c_s
+        return self.out(lstm_out), h_s, c_s
 
 
 class DoubleConvLSTM(nn.Module):
@@ -88,9 +80,5 @@ class DoubleConvLSTM(nn.Module):
         x = self.conv2(x)
         x = x.permute(0, 2, 1)
         lstm_out, (h_s, c_s) = self.lstm(x, state)
-        outs = []
-        for ts in range(lstm_out.size(1)):
-            outs.append(self.out(lstm_out[:,ts,:]))
-
-        return torch.stack(outs, dim=1), h_s, c_s
+        return self.out(lstm_out), h_s, c_s
     
