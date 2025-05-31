ad0fd841b39bba2854192713b5b035539af6916e
witmus <witold.muszynski123@wp.pl>
2025-06-01 00:18:23 +0200

    fix

--- kd_teachers_inference.py
+++ kd_teachers_inference.py
@@ -37,13 +37,20 @@ y = crunch[data_start:data_end]
 lstm_teacher_path = 'models/knowledge_distillation/teachers/lstm_checkpoint_19.pth'
 wavenet_teacher_path = 'models/knowledge_distillation/teachers/wavenet_checkpoint_19.pth'
 
+if torch.cuda.is_available():
+    is_cuda = True
+    device = "cuda"
+else:
+    is_cuda = False
+    device = "cpu"
+
 lstm_teacher = WindowLSTM(
     n_conv_outs=lstm_channels,
     s_kernel=lstm_kernel_size,
     n_stride=lstm_stride,
     n_hidden=lstm_hidden_size,
     n_layers=lstm_layers
-)
+).to(device)
 
 lstm_teacher.load_state_dict(
     torch.load(lstm_teacher_path,weights_only=True)
@@ -54,7 +61,7 @@ wavenet_teacher = WaveNet(
     dilation_depth=wn_dilation_depth,
     num_repeat=wn_repeats,
     kernel_size=wn_kernel_size
-)
+).to(device)
 
 wavenet_teacher.load_state_dict(
     torch.load(wavenet_teacher_path,weights_only=True)
@@ -63,14 +70,14 @@ wavenet_teacher.load_state_dict(
 lstm_teacher.eval()
 wavenet_teacher.eval()
 
-lstm_dataloader = get_sw_dataloader(x, lstm_window_size, lstm_batch_size)
-wavenet_dataloader = get_wavenet_dataloader(x, wn_steps, wn_batch_size)
+lstm_dataloader = get_sw_dataloader(x, lstm_window_size, lstm_batch_size, is_cuda)
+wavenet_dataloader = get_wavenet_dataloader(x, wn_steps, wn_batch_size, is_cuda)
 
 lstm_result = torch.tensor([])
 with torch.no_grad():
     for i,x_t in enumerate(lstm_dataloader):
         print(i,'/',len(lstm_dataloader),end='\r')
-        pred,_,_ = lstm_teacher(x_t)
+        pred,_,_ = lstm_teacher(x_t.to(device))
         lstm_result = torch.concat((lstm_result, pred.detach().flatten()))
 
 print(x.shape)
@@ -80,7 +87,7 @@ wavenet_result = torch.tensor([])
 with torch.no_grad():
     for i,x_t in enumerate(wavenet_dataloader):
         print(i+1,'/',len(wavenet_dataloader),end='\r')
-        pred = wavenet_teacher(x_t)
+        pred = wavenet_teacher(x_t.to(device))
         pred = pred[:,:,-wn_steps:]
         wavenet_result = torch.concat((wavenet_result, pred.detach().flatten()))
 
